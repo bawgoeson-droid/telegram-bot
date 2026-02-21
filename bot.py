f3a4dd602e30215780abc6b19e34376cefa8b06c
bawgoeson-droid <bawgoeson@gmail.com>
2026-02-21 14:52:10 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,16 @@
+import os
+import telebot
 
+TOKEN = os.getenv("BOT_TOKEN")
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=["start"])
+def start(msg):
+    bot.send_message(msg.chat.id, "Бот запущен и работает 🚀")
+
+@bot.message_handler(func=lambda m: True)
+def echo(msg):
+    bot.send_message(msg.chat.id, "Сообщение получено ✅")
+
+print("Bot started...")
+bot.infinity_polling()
